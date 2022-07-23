1e14a4a7231b0a4efdc4c07eacde1dc1d5fa5235
Dexvo <63011374+Dexvoo@users.noreply.github.com>
2022-07-23 18:06:04 +0100

    📝 | Updated README | Added Checks for Config

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # IN-ServerStatus
- 
+
+A discord bot that interacts with [cfx-api](https://github.com/PABLO-1610/cfx-api), Shows with a embed that updates every so often.
+
+**How to install**
+
+```bash
+npm install
+```
+
+Create a `.env` file and use the config below, Please fill in as much as you can.
+
+```bash
+# Discord Bot Token
+Token=''
+# Your Guild Information
+ClientID=''
+GuildID=''
+# Embed Config
+ThumnailImage='https://imgur.com/0KIFmOl.gif'
+EmbedColour='#a082ff'
+FooterText='• IND 2022'
+FooterImage='https://imgur.com/0KIFmOl.gif'
+# FiveM Server Status
+FiveMEmbedUpdateTime=60 # In Seconds
+FiveMServerID='lm6l84' # https://keymaster.fivem.net/
+# Fill in this once you have got your message you want to embed
+FiveMChannelID=''
+FiveMMessageID=''
+DonationWebsite='https://ind.tebex.io'
+```
+
+Discord Commands Register
+
+```bash
+npm run commands
+```
+
+Starting Bot
+
+```bash
+npm run dev
+```
+
+Discord Command in the channel you wish
+
+```bash
+/serverstatus
+```
+
+Copy the channel id and message id, place it the config. Save any .js file and it will be running!
--- src/events/ServerStatus.js
+++ src/events/ServerStatus.js
@@ -11,54 +11,63 @@ module.exports = {
         {
             var liveStatus = await cfx.fetchStatus()
             var liveServer = await cfx.fetchServer(FiveMServerID)
-            const liveChannelName = client.channels.cache.find(channel => channel.id == (FiveMChannelID))
-            if (liveServer !== undefined)
+            if (FiveMChannelID !== '' || FiveMMessageID !== '')
             {
-                liveChannelName.messages.fetch(FiveMMessageID).then((message) =>
+                const liveChannelName = client.channels.cache.find(channel => channel.id == (FiveMChannelID))
+                if (liveServer !== undefined)
                 {
-                    const liveStatusMessage = new EmbedBuilder()
-                        .setThumbnail(ThumnailImage)
-                        .setColor(EmbedColour)
-                        .addFields(
-                            { name: '• Server Status •', value: 'Online ✅', inline: true },
-                            { name: '• CFX Status •', value: liveStatus.everythingOk ? "Online ✅" : "Issues ⚠️", inline: true },
-                            { name: '• Conected Players •', value: `${liveServer.playersCount} / ${liveServer.maxPlayers}`, inline: true },
-                        )
-                        .setFooter({ text: '• Last update: ', iconURL: FooterImage })
-                        .setTimestamp()
-
-                    const livePlayerNames = []
-                    for (var player in liveServer.players)
+                    liveChannelName.messages.fetch(FiveMMessageID).then((message) =>
                     {
-                        livePlayerNames.push(`ID: ${liveServer.players[player].id} | ${liveServer.players[player].name}\n`)
-                    }
+                        const liveStatusMessage = new EmbedBuilder()
+                            .setThumbnail(ThumnailImage)
+                            .setColor(EmbedColour)
+                            .addFields(
+                                { name: '• Server Status •', value: 'Online ✅', inline: true },
+                                { name: '• CFX Status •', value: liveStatus.everythingOk ? "Online ✅" : "Issues ⚠️", inline: true },
+                                { name: '• Conected Players •', value: `${liveServer.playersCount} / ${liveServer.maxPlayers}`, inline: true },
+                            )
+                            .setFooter({ text: '• Last update: ', iconURL: FooterImage })
+                            .setTimestamp()
 
-                    for (let i = 0; i < livePlayerNames.length; i += 10)
-                    {
-                        liveStatusMessage.addFields({ name: 'Player Information', value: `${livePlayerNames.sort().slice(i, i + 10).join('')}`, inline: true },)
-                    }
-                    message.edit({ embeds: [liveStatusMessage] });
-                })
-                client.user?.setActivity(`Players: ${liveServer.playersCount}/${liveServer.maxPlayers}`, { type: 'WATCHING' });
-            } else
-            {
-                liveChannelName.messages.fetch(FiveMMessageID).then((message) =>
+                        const livePlayerNames = []
+                        for (var player in liveServer.players)
+                        {
+                            livePlayerNames.push(`ID: ${liveServer.players[player].id} | ${liveServer.players[player].name}\n`)
+                        }
+
+                        for (let i = 0; i < livePlayerNames.length; i += 10)
+                        {
+                            liveStatusMessage.addFields({ name: 'Player Information', value: `${livePlayerNames.sort().slice(i, i + 10).join('')}`, inline: true },)
+                        }
+                        message.edit({ embeds: [liveStatusMessage] });
+                    })
+                    client.user?.setActivity(`Players: ${liveServer.playersCount}/${liveServer.maxPlayers}`, { type: 'WATCHING' });
+                }
+                else
                 {
-                    const liveStatusMessage = new EmbedBuilder()
-                        .setThumbnail('https://imgur.com/0KIFmOl.gif')
-                        .setColor('#a082ff')
-                        .addFields(
-                            { name: '• Server Status •', value: 'Offline ❌', inline: true },
-                            { name: '• Conected Players •', value: `0/0`, inline: true },
-                            { name: '• Restart Time •', value: `3PM BST (London)`, inline: true },
-                            { name: 'Player Information', value: `None to Display`, inline: false },
-                        )
-                        .setFooter({ text: '• Last update: ', iconURL: FooterImage })
-                        .setTimestamp()
-                    message.edit({ embeds: [liveStatusMessage] });
-                })
+                    liveChannelName.messages.fetch(FiveMMessageID).then((message) =>
+                    {
+                        const liveStatusMessage = new EmbedBuilder()
+                            .setThumbnail('https://imgur.com/0KIFmOl.gif')
+                            .setColor('#a082ff')
+                            .addFields(
+                                { name: '• Server Status •', value: 'Offline ❌', inline: true },
+                                { name: '• Conected Players •', value: `0/0`, inline: true },
+                                { name: '• Restart Time •', value: `3PM BST (London)`, inline: true },
+                                { name: 'Player Information', value: `None to Display`, inline: false },
+                            )
+                            .setFooter({ text: '• Last update: ', iconURL: FooterImage })
+                            .setTimestamp()
+                        message.edit({ embeds: [liveStatusMessage] });
+                    })
+                }
+                setTimeout(liveServerStatus, 1000 * FiveMEmbedUpdateTime)
             }
-            setTimeout(liveServerStatus, 1000 * FiveMEmbedUpdateTime)
+            else
+            {
+                console.log('Please refer to the read.md for the the correct instalation.')
+                return
+            }
         }
         liveServerStatus()
     }
